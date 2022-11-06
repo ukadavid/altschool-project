271dd285c371cb20aacfa3897159306f9449bb21
Tobechukwu David Uka <54605363+ukadavid@users.noreply.github.com>
2022-11-06 13:32:42 +0100

    UI design II

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "../styles/ErrorPage.css";
 import { Link } from "react-router-dom";
-import AltSchoolLogo from "../images/AltSchoolLogo.png";
 import ErrorImage from "../images/ErrorImage.png";
 
 const ErrorPage = () => {
   return (
     <div className="error">
       <div className="logo nav">
-        <img alt="AltSchool-Logo" src={AltSchoolLogo} />
         <Link to="/">
           <p>Return to Homepage</p>
         </Link>
